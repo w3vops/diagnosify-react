a2829b763baf8687ff6ce74af9cb66c918b15ed5
Nodenkey <nodenkey@gmail.com>
2020-05-17 01:37:40 +0000

    added sign up page

--- src/components/button/customButton.jsx
+++ src/components/button/customButton.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./customButton.style.css";
 import {Link} from "react-router-dom";
+import Google from "../../assets/images/google.png";
 
 
 const CustomButton = ({text, button_type, icon, link}) => {
@@ -10,6 +11,11 @@ const CustomButton = ({text, button_type, icon, link}) => {
         return <Link to={link}><button className="diagnosify-button button2"><i className={icon}/>{text}</button></Link>
     }else if (button_type === 'type3'){
         return <Link to={link}><button className="diagnosify-button button3"><i className={icon}/>{text}</button></Link>
+    }else if (button_type === 'submit'){
+        return <button className="diagnosify-button button" type='submit'>{text}</button>
+    }else if (button_type === 'google'){
+        return <button className="diagnosify-button button4"><img src={Google} alt="google icon" className='google'/>{text}</button>
+
     }
 };
 export default CustomButton;
